8c12e8431fbca51373b8408cbc2076684936d33f
Christoph <108698633+W-Christoph@users.noreply.github.com>
2026-02-13 18:24:22 +0100

    Cleanup code

--- cozycast-server/src/main/groovy/com/github/vorlent/cozycastserver/interface/MediaManager.groovy
+++ cozycast-server/src/main/groovy/com/github/vorlent/cozycastserver/interface/MediaManager.groovy
@@ -7,7 +7,5 @@ class WorkerMediaInfo {
 
 interface MediaManager {
     WorkerMediaInfo setupWorker(String roomName)
-    void releaseWorker(String roomName)
     String getPlayerToken(String roomName, String identity, String nickname)
-    void processWorkerAnswer(WorkerSession worker, String roomName, String sdp)
 }
